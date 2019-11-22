ec4230150a4cf9645365941eda5238ea9158dd00
Lee TaeYong <joylty94@naver.com>
2019-11-22 22:49:38 +0900

    code 정리

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ import v1Route from './routes/v1'
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -22,12 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/v1', v1Route)
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
   let apiError = err
 
